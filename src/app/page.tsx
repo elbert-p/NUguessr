cff707dfbeb18485bc82e73c2223fae2ad4616ac
elbert-p <elbert.p@northeastern.edu>
2025-02-08 01:22:49 -0500

    updated homepage spacing and button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,14 +20,14 @@ export default function HomePage() {
       </div>
 
       <main className="text-center z-10 relative px-6 py-8 w-full max-w-4xl mx-auto mt-4 bg-gray-800/60 rounded-xl backdrop-blur-[2px]">
-        <div className="space-y-4 mb-12">
+        <div className="space-y-4 mb-8">
           <p className=" text-5xl font-[1000] text-white" style={{ textShadow: "0 2px 4px rgba(0,0,0,0.5)" }}>
             Think you know your school well?
           </p>
           <p className="text-4xl font-[1000] text-white" style={{ textShadow: "0 2px 4px rgba(0,0,0,0.5)" }}>
             Want to familiarize yourself with the campus?
           </p>
-          <div>
+          <div className="pt-4">
           <Link
             href="/play"
             className="text-white  inline-block mb-8 bg-red-600 hover:bg-red-700 py-4 px-20 rounded-full text-2xl shadow-lg transition-all hover:scale-105"
