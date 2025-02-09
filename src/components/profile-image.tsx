a265b3410e542e97a63185389078b32e14b3ef62
elbert-p <elbert.p@northeastern.edu>
2025-02-09 05:00:52 -0500

    for vercel

--- src/components/profile-image.tsx
+++ src/components/profile-image.tsx
@@ -1,24 +1,20 @@
-import "@fontsource/open-sans"
-import '@fontsource/titillium-web'
-import { useUser } from "../../hooks/use-user"
-import { Icons } from "@/components/icons"
+import "@fontsource/open-sans";
+import "@fontsource/titillium-web";
+import { useUser } from "../../hooks/use-user";
+import { Icons } from "@/components/icons";
 
-export const profileImage = () => {
-    const { user } = useUser()
-    
-    if (user?.email) {
-        return (
-        <img
-            src={user?.user_metadata.avatar_url}
-            alt="Profile image"
-            className="w-8 h-8 rounded-full"
-        />
-        )
-    }
-    else {
-        return (
-            <Icons.google style={{ width: "32px", height: "32px" }} />
-          );
-          
-    }
-}
+export const ProfileImage = () => {
+  const { user } = useUser();
+
+  if (user?.email) {
+    return (
+      <img
+        src={user?.user_metadata.avatar_url}
+        alt="Profile image"
+        className="w-8 h-8 rounded-full"
+      />
+    );
+  } else {
+    return <Icons.google style={{ width: "32px", height: "32px" }} />;
+  }
+};
